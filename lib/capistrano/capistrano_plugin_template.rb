8cebe2d9a01f309adf8b46dd76d84ac71c543945
dspaeth-faber <d.spaeth@faber.de>
2014-06-02 08:24:26 +0200

    changes to satisfy rubocop

--- lib/capistrano/capistrano_plugin_template.rb
+++ lib/capistrano/capistrano_plugin_template.rb
@@ -1,6 +1,6 @@
 require 'capistrano/template'
 
 # don't pollute global namespace
-self.extend Capistrano::Template::Helpers::DSL
+extend Capistrano::Template::Helpers::DSL
 
 import File.join(__dir__, 'template', 'tasks', 'template_defaults.rake')
