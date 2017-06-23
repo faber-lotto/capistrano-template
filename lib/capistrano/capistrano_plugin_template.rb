46d630973d4cb05b4860eaf8604d70817de67df0
Dieter Späth <shad0wrunner@gmx.de>
2017-06-23 19:33:18 +0200

    fix an issue with printer backend refs: #13 (#14)
    * fix an issue with printer backend refs: #13
    * Bumped version. Added changlog informations.

--- lib/capistrano/capistrano_plugin_template.rb
+++ lib/capistrano/capistrano_plugin_template.rb
@@ -6,4 +6,12 @@ extend Capistrano::Template::Helpers::DSL
 
 SSHKit::Backend::Netssh.send(:include, Capistrano::Template::Helpers::DSL)
 
+# rubocop: disable  Lint/HandleExceptions
+begin
+  require 'sshkit/backend/printer'
+  SSHKit::Backend::Printer.send(:include, Capistrano::Template::Helpers::DSL)
+rescue LoadError
+end
+# rubocop: enable  Lint/HandleExceptions
+
 import File.join(__dir__, 'template', 'tasks', 'template_defaults.rake')
--- lib/capistrano/template/version.rb
+++ lib/capistrano/template/version.rb
@@ -1,5 +1,5 @@
 module Capistrano
   module Template
-    VERSION = '0.0.8'
+    VERSION = '0.0.9'
   end
 end
