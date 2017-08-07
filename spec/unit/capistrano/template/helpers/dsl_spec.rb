5303f80a0dd627c1481d67a679114882136515aa
Mike Crockett <github@mmcrockett.com>
2017-08-07 14:16:17 -0500

    Adding hash-style call to template().

--- spec/unit/capistrano/template/helpers/dsl_spec.rb
+++ spec/unit/capistrano/template/helpers/dsl_spec.rb
@@ -32,6 +32,10 @@ module Capistrano
               data.fetch(*args)
             end
 
+            def dry_run?
+              true
+            end
+
             def _paths_factory
               lambda do |*args|
                 PathsLookup.new(*args).tap do |pl|
@@ -58,7 +62,30 @@ module Capistrano
             expect { subject.template(template_name) }.to raise_error(ArgumentError, /template #{template_name} not found Paths/)
           end
         end
+
+        describe '#template_p' do
+          it 'makes underlying call to template' do
+            p = {
+              :from => 'blah',
+              :to   => 'to',
+              :mode => '0744',
+              :user => 'bob',
+              :grp  => 'users',
+              :locals => {
+                :testkey => 'testval'
+              }
+            }
+            expect(subject).to receive(:template).with(template_name, p[:to], p[:mode], p[:user], p[:group], locals: p[:locals])
+            subject.template_p(template_name, p)
+          end
+
+          it 'has default values' do
+            expect(subject).to receive(:template).with(template_name, nil, Capistrano::Template::Helpers::DSL::MODE_DEFAULT, nil, nil, locals: {})
+            subject.template_p(template_name)
+          end 
+        end
       end
+
     end
   end
 end
